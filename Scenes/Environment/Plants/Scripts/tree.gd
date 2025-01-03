929158568f6c51209d82a928421e715367c96cf7
Nick Petruccelli <NickPetro18@gmail.com>
2025-01-03 16:42:00 -0500

    added time to deconstruction

--- Scenes/Environment/Plants/Scripts/tree.gd
+++ Scenes/Environment/Plants/Scripts/tree.gd
@@ -7,7 +7,6 @@ func _ready() -> void:
 func cut_down(character_stats: Dictionary) -> bool:
 	var arbory_mod = character_stats["skills"]["arbory"] / 10.0
 	health -= 10 + (character_stats["strength"] * arbory_mod)
-	print(health)
 	if health <= 0:
 		var scene_manager = get_tree().get_first_node_in_group("scenemanager")
 		scene_manager.add_ground_item(Cords.get_map_from_global(self.global_position), item_id)
--- Scenes/GameObjects/Characters/Scripts/Tasks/deconstruct.gd
+++ Scenes/GameObjects/Characters/Scripts/Tasks/deconstruct.gd
@@ -4,6 +4,9 @@ class_name Deconsturct
 @onready var character: CharacterBody2D = $"../../.."
 @onready var pathfinding: Pathfinding = $"../../../Pathfinding"
 
+var time_to_decon = 2000
+var time_since_decon_start = null
+
 func execute(args: Array) -> void:
 	character.goal_pos = args[0]
 
@@ -13,13 +16,20 @@ func update() -> void:
 func physics_update() -> void:
 	if character.goal_pos == null:
 		return
+	if character.global_position.distance_to(character.goal_pos) < 18:
+		if time_since_decon_start == null:
+			time_since_decon_start = Time.get_ticks_msec()
+		if Time.get_ticks_msec() - time_since_decon_start < time_to_decon:
+			return
+		var scene_manager = get_tree().get_first_node_in_group("scenemanager")
+		scene_manager.build_layer.deconstruct_build(character.goal_pos)
+		exit()
+		return
 	var next_node = pathfinding.next_node(character.global_position)
 	var vel = character.global_position.direction_to(next_node)
 	character.velocity = vel
 	character.move_with_vel()
-	if character.global_position.distance_to(character.goal_pos) < 18:
-		character.build_layer.deconstruct_build(character.goal_pos)
-		exit()
+	
 
 func exit() -> void:
 	character.goal_pos = null
