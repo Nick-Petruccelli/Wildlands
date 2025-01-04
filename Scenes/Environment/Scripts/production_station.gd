2e2c1994f0e386906c31eaf4842ff3e328968796
Nick Petruccelli <NickPetro18@gmail.com>
2025-01-04 11:03:20 -0500

    started prod station ui

--- Scenes/Environment/Scripts/production_station.gd
+++ Scenes/Environment/Scripts/production_station.gd
@@ -6,7 +6,6 @@ class_name ProductionStation
 
 var type_id: int = -1
 var type_name: String
-var producables: Dictionary
 
 func load_data(id: int) -> void:
 	var data = get_tree().get_first_node_in_group("gamedata").environment_data[id]
@@ -15,9 +14,18 @@ func load_data(id: int) -> void:
 	var tex = load(data["texture"])
 	sprite_2d.texture = tex
 	collision_shape_2d.shape.set("size", tex.get_size())
-	#var item_data = get_tree().get_first_node_in_group("gamedata").item_data
-	
-	
+	open_ui()
 
 func open_ui() -> void:
-	pass
+	var item_data = get_tree().get_first_node_in_group("gamedata").item_data
+	var producables_list = get_tree().get_first_node_in_group("producableslist")
+	for child in producables_list.get_children():
+		child.queue_free()
+	for id in item_data:
+		print(item_data[id])
+		if !item_data[id].has("prod_station") or item_data[id]["prod_station"] != type_id:
+			continue
+		var btn = ConstructionListItem.new()
+		btn.text = item_data[id]["name"]
+		btn.data = item_data[id]
+		producables_list.add_child(btn)
