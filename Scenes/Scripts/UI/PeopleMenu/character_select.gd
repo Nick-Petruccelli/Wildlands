9c67a21b2edd2a366dfb3e7d3909d87ac6092d6c
Nick Petruccelli <NickPetro18@gmail.com>
2025-01-06 10:41:31 -0500

    added equipment to character veiwer

--- Scenes/Scripts/UI/PeopleMenu/character_select.gd
+++ Scenes/Scripts/UI/PeopleMenu/character_select.gd
@@ -2,9 +2,16 @@ extends VBoxContainer
 
 func load_characters() -> void:
 	var characters = get_tree().get_first_node_in_group("scenemanager").characters.get_children()
+	var first: bool = true
+	for btn in self.get_children():
+		btn.queue_free()
 	for character in characters:
 		var btn = CharacterButton.new()
 		btn.text = character.name
 		btn.character = character
 		btn.display = $"../CharacterDisplay"
 		self.add_child(btn)
+		if first:
+			btn._on_click()
+			first = false
+	
--- Scenes/character_display.gd
+++ Scenes/character_display.gd
@@ -1,14 +1,22 @@
 extends VBoxContainer
 
 @onready var _name: Label = $Name
-@onready var picture: TextureRect = $Picture
+@onready var picture: TextureRect = $HBoxContainer2/Picture
 @onready var stats: GridContainer = $HBoxContainer/Stats
+@onready var head_item: Label = $HBoxContainer2/VBoxContainer/GridContainer/HeadItem
+@onready var body_item: Label = $HBoxContainer2/VBoxContainer/GridContainer/BodyItem
+@onready var legs_item: Label = $HBoxContainer2/VBoxContainer/GridContainer/LegsItem
+@onready var main_hand_item: Label = $HBoxContainer2/VBoxContainer/GridContainer/MainHandItem
+@onready var off_hand_item: Label = $HBoxContainer2/VBoxContainer/GridContainer/OffHandItem
+@onready var character_display: VBoxContainer = $"."
 
 
 func show_character(character) -> void:
 	load_name(character.name)
 	load_picture(character.animated_sprite_2d)
+	load_equipment(character.equipment)
 	load_stats(character.stats)
+	character_display.visible = true
 	
 	
 func load_name(new_name: String) -> void:
@@ -20,14 +28,25 @@ func load_picture(char_sprite: AnimatedSprite2D) -> void:
 	var spriteFrames: SpriteFrames = char_sprite.get_sprite_frames()
 	picture.texture = spriteFrames.get_frame_texture(animationName, frameIndex)
 
+func load_equipment(char_equip: Equipment) -> void:
+	var item_data = get_tree().get_first_node_in_group("gamedata").item_data
+	var equip = char_equip._equipment
+	head_item.text = item_data[equip["head"]]["name"] if equip["head"] > 0 else "none"
+	body_item.text = item_data[equip["body"]]["name"] if equip["body"] > 0 else "none"
+	legs_item.text = item_data[equip["legs"]]["name"] if equip["legs"] > 0 else "none"
+	main_hand_item.text = item_data[equip["main_hand"]]["name"] if equip["main_hand"] > 0 else "none"
+	off_hand_item.text = item_data[equip["off_hand"]]["name"] if equip["off_hand"] > 0 else "none"
+
 func load_stats(char_stats: CharacterStats) -> void:
-	var stats = char_stats.stats
-	for stat in stats:
+	for stat in stats.get_children():
+		stat.queue_free()
+	var stats_dict = char_stats.stats
+	for stat in stats_dict:
 		if stat == "skills":
 			break
-		add_stat(stat, stats[stat])
-	for skill in stats["skills"]:
-		add_stat(skill, stats["skills"][skill])
+		add_stat(stat, stats_dict[stat])
+	for skill in stats_dict["skills"]:
+		add_stat(skill, stats_dict["skills"][skill])
 
 		
 func add_stat(_name: String, level: int) -> void:
