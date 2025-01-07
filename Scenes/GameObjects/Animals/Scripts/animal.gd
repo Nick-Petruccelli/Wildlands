99dc8b418d49b2377307ae9696cf17710e91beaf
Nick Petruccelli <NickPetro18@gmail.com>
2025-01-07 12:36:47 -0500

    added animal flee on attacked

--- Scenes/GameObjects/Animals/Scripts/animal.gd
+++ Scenes/GameObjects/Animals/Scripts/animal.gd
@@ -17,7 +17,7 @@ func _physics_process(delta: float) -> void:
 	if !paniced:
 		wander()
 	else:
-		pass
+		flee()
 
 func wander() -> void:
 	if Time.get_ticks_msec() - move_dir_last_change > 2000:
@@ -25,6 +25,19 @@ func wander() -> void:
 		move_dir_last_change = Time.get_ticks_msec()
 	velocity  = move_dir * stats["speed"]
 	#print(move_dir)
+	move_and_slide()
+	
+func flee() -> void:
+	var characters: Array[Node] = get_tree().get_first_node_in_group("scenemanager").characters.get_children()
+	var avoid_vec = Vector2(0, 0)
+	for character in characters:
+		if global_position.distance_to(character.global_position) > 512:
+			continue
+		avoid_vec += global_position - character.global_position
+	if avoid_vec == Vector2(0, 0):
+		paniced = false
+	move_dir = avoid_vec.normalized()
+	velocity = move_dir * stats["speed"]
 	move_and_slide()
 	
 func take_damage(damage: int) -> void:
@@ -37,5 +50,4 @@ func is_dead() -> bool:
 func get_rand_dir() -> Vector2:
 	var x = randf_range(-.25, .25)
 	var y = randf_range(-.25, .25)
-	print(x, y)
 	return Vector2(x, y)
